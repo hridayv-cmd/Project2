34b68ba4ef1c807577d8257a76251713a8f3bef0
Hriday <hridayvermani12@gmail.com>
2026-07-08 12:57:36 -0400

    Add new text file which contains File I/O, Exceptions, & JSON

---
+++ storage.py
@@ -0,0 +1,7 @@
+"""
+Program Name: QuestLog Storage Module
+Author: Hriday Vermani
+Purpose: Handles file persistence using JSON serialization and error handling.
+Resources Used: Python Crash Course Chapter 10.
+Date: July 8, 2026
+"""
