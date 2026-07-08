14ed1540e134a08ee5f50a61782ee965e6240525
Hriday <hridayvermani12@gmail.com>
2026-07-08 12:29:12 -0400

    Add doctring for OOP text file

---
+++ character.py
@@ -0,0 +1,7 @@
+"""
+Program Name: QuestLog Character Module
+Author: Hriday Vermani
+Purpose: Defines the character classes and inventory system.
+Resources Used: Python Crash Course Chapters 9 (OOP) and 8 (Functions).
+Date: July 8, 2026
+"""
