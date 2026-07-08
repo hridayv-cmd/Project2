5ba21d4459b795f95f1def79cfacbaff5eb9dbb4
Hriday <hridayvermani12@gmail.com>
2026-07-08 14:17:18 -0400

    Add doc string for main.py handles the main components of the game

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Program Name: QuestLog Main Application
+Author: Hriday vermani
+Purpose: Houses the core game loop and user interaction terminal interface.
+Resources Used: Python Crash Course Chapter 8.
+Date: July 8, 2026
+"""
